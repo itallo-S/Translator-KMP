fb6c3a2f14cdd2d21341a8755fc699db349b816f
itallo-S <itallo_dsilva@hotmail.com>
2025-06-03 23:18:46 -0300

    Config ios

---
+++ iosApp/iosApp/translate/presentation/components/LanguageDropDownItem.swift
@@ -0,0 +1,36 @@
+//
+//  LanguageDropDownItem.swift
+//  iosApp
+//
+//  Created by Itallo da Silva Alves on 03/06/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+import shared
+
+struct LanguageDropDownItem: View {
+    var language: UiLanguage
+    var onClick: () -> Void
+    var body: some View {
+        Button(action: onClick) {
+            HStack {
+                if let image = UIImage(named: language.imageName.lowercased()) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .padding(.trailing, 5)
+                    Text(language.language.langName)
+                        .foregroundColor(Color.textBlack)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LanguageDropDownItem(
+        language: UiLanguage(language: .german, imageName: "german"),
+        onClick: {}
+    )
+}
